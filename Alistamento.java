790b28d630f8c0333bfa05c23d2a47bbc3026f6d
Emanuelle <emanuelleliisboa@gmail.com>
2026-03-24 20:03:56 -0300

    Atividades referentes às duas práticas realizadas em sala.

---
+++ Alistamento.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Alistamento {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o nome do(a) recruta: ");
+        String nome = scanner.nextLine().toUpperCase();
+        System.out.println("Olá, " + nome + "!");
+
+        System.out.println("Digite o seu peso");
+        double peso = scanner.nextDouble();
+        System.out.println("Seu peso é: " + peso + "kg.");
+
+        if (peso < 50){
+            System.out.println("Você está abaixo do peso ideal para o alistamento.");
+        } else if (peso >= 50 && peso <= 100){
+            System.out.println("Você está dentro do peso ideal para o alistamento.");
+        } else {
+            System.out.println("Você está acima do peso ideal para o alistamento.");
+        }
+        scanner.close();
+    }
+}
